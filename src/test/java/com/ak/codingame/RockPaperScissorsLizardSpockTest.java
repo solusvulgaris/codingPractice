bf12b995424f7b13daf76db6189b272dda218fd7
Krauchanka, Alexandra <Alexandra.Krauchank@wolterskluwer.com>
2025-01-07 22:04:23 +0200

    codingame: RockPaperScissorsLizardSpock (test)

--- src/test/java/com/ak/codingame/RockPaperScissorsLizardSpockTest.java
+++ src/test/java/com/ak/codingame/RockPaperScissorsLizardSpockTest.java
@@ -1,41 +1,41 @@
 package com.ak.codingame;
 
 import com.ak.codingame.RockPaperScissorsLizardSpock.Player;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
 public class RockPaperScissorsLizardSpockTest {
+
     public static Stream<Arguments> testData() {
         return Stream.of(
-                Arguments.of(
-                        new String[]{
-                                "8",
-                                "4", "R",
-                                "1", "P",
-                                "8", "P",
-                                "3", "R",
-                                "7", "C",
-                                "5", "S",
-                                "6", "L",
-                                "2", "L"},
-                        new String[]{
-                                "2",
-                                "6 5 1"
-                        })
+            Arguments.of(
+                new String[]{
+                    "8",
+                    "4", "R",
+                    "1", "P",
+                    "8", "P",
+                    "3", "R",
+                    "7", "C",
+                    "5", "S",
+                    "6", "L",
+                    "2", "L"},
+                new String[]{
+                    "2",
+                    "6 5 1"
+                })
         );
     }
 
 
     @ParameterizedTest
     @MethodSource("testData")
-    @DisplayName("")
+    @DisplayName("RockPaperScissorsLizardSpockTest")
     void longestCommonSequenceTest(String[] input, String[] result) {
         int N = Integer.valueOf(input[0]); //number of participants
         List<Player> players = new ArrayList<>();
